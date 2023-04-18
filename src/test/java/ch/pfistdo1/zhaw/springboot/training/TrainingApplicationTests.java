2db9be84b9a64e02fba3450c6e45bfded6fef85b
pfistdo <pfistdo1@students.zhaw.ch>
2023-04-18 20:12:53 +0200

    first commit

---
+++ src/test/java/ch/pfistdo1/zhaw/springboot/training/TrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.pfistdo1.zhaw.springboot.training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
